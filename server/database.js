a396c4078aedeab8ac808ad5d7200edb91541954
Eberhard Schneider <mail@eberhardschneider.info>
2018-02-11 19:26:37 +0100

    change mongodb to local

--- server/database.js
+++ server/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-// import User from './models/User';
-// import Room from './models/Room';
 
-mongoose.connect('mongodb://EbbSnyder:mVTIj6HOiXkXZssT@cluster0-shard-00-00-vhz9c.mongodb.net:27017,cluster0-shard-00-01-vhz9c.mongodb.net:27017,cluster0-shard-00-02-vhz9c.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
-  () => {
-    console.log('Connected');
-  });
+// mongoose.connect('mongodb://EbbSnyder:mVTIj6HOiXkXZssT@cluster0-shard-00-00-vhz9c.mongodb.net:27017,cluster0-shard-00-01-vhz9c.mongodb.net:27017,cluster0-shard-00-02-vhz9c.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+//   () => {
+//     console.log('Connected');
+//   });
+
+mongoose.connect('mongodb://localhost:27017/test', () => {
+  console.log('Connected to local mongodb database');
+});
